24b357a15194d68b86f9f118021ac92d8f22f90b
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-23 13:35:18 +0100

    init

---
+++ BackEnd/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '824e25712efbe39106578afb59ddc3f1'),
+    },
+  },
+});
