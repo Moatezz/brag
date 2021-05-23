56fdb365e8b03175f207d9192b3b92efcaf26162
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-23 22:19:15 +0100

    added theme.js for future use

---
+++ client/src/components/Theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core';
+import { lightBlue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue,
+  },
+});
