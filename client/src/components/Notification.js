64180a62f69057f4e49fcd9f3d9f3341dfc26e3c
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-23 17:23:34 +0100

    added notification item till further devs

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,8 +1,29 @@
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import {
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Paper,
+  Typography,
+  makeStyles,
+  Grid,
+} from '@material-ui/core';
 import React, { useState } from 'react';
 import { MdNotificationsNone } from 'react-icons/md';
-
+const useStyle = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(1),
+  },
+  text: {
+    fontSize: theme.spacing(1.5),
+  },
+  date: {
+    fontSize: theme.spacing(1.2),
+    opacity: 0.5,
+  },
+}));
 function Notification(props) {
+  const classes = useStyle();
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = (event) => {
@@ -32,7 +53,28 @@ function Notification(props) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem>Hi</MenuItem>
+        <MenuItem>
+          <Paper variant="outlined" className={classes.container}>
+            <Grid
+              justifyContent="space-between"
+              alignItems="center"
+              container
+              spacing={2}
+            >
+              <Grid item>
+                <Avatar variant="rounded">N</Avatar>
+              </Grid>
+              <Grid item>
+                <Typography className={classes.text}>
+                  You Have received some new notification
+                </Typography>
+                <Typography className={classes.date}>
+                  Sun 12th, 12:54 AM
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </MenuItem>
       </Menu>
     </>
   );
