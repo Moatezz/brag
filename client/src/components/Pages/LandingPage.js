f8d4b7652b3ee652d86edc6b709ccd91c3a05371
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-25 00:27:21 +0100

    1/2 ready

--- client/src/components/Pages/LandingPage.js
+++ client/src/components/Pages/LandingPage.js
@@ -1,7 +1,36 @@
+import { Box, Divider, makeStyles, Typography } from '@material-ui/core';
+import Logo from '../Logo';
 import React from 'react';
 
+const useStyle = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'black',
+  },
+  typography: {
+    color: 'white',
+    fontFamily: "'Cairo', sans-serif",
+  },
+  divider: {
+    color: 'white',
+  },
+}));
+
 function LandingPage() {
-  return <div></div>;
+  const classes = useStyle();
+  return (
+    <Box className={classes.root}>
+      <Logo variant="big" />
+      <Typography className={classes.typography} variant="h2">
+        THE START IS COMING
+      </Typography>
+      <Divider className={classes.divider} />
+      <Typography className={classes.typography} variant="h4">
+        Lorem ipsum in the name of all of those who died in the battle of
+        algeria
+      </Typography>
+      -
+    </Box>
+  );
 }
 
 export default LandingPage;
