2b761fa734e13ac9d1576fe407d4dd511f8dd37c
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-24 18:12:10 +0100

    custom api endpoint

--- cms/api/post/controllers/post.js
+++ cms/api/post/controllers/post.js
@@ -1,8 +1,19 @@
-'use strict';
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+  async kill(ctx) {
+    let entities;
+    let some = entities[0].group.name;
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      entities = await strapi.services.post.find({ some: 1 });
+    }
+    console.log(entities[0].group.name);
+    return entities;
+  },
+};
