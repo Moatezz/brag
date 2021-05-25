e6f6578098168999981ce6dea30cd46f04a45f77
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-25 13:51:35 +0100

    so far so good

--- client/src/components/DottedMenu.js
+++ client/src/components/DottedMenu.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
 
 function DottedMenu() {
-  return <div></div>;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <BiDotsVerticalRounded />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <Typography> Hi</Typography>
+        </MenuItem>
+      </Menu>
+    </>
+  );
 }
 
 export default DottedMenu;
