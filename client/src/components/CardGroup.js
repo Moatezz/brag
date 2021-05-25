3335b50db224b6d3271122a02d28836cf3408962
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-25 11:50:05 +0100

    added partial boilerplate

--- client/src/components/CardGroup.js
+++ client/src/components/CardGroup.js
@@ -1,7 +1,18 @@
+import { Avatar, Box, Paper, Typography, Button } from '@material-ui/core';
 import React from 'react';
 
 function CardGroup() {
-  return <div></div>;
+  return (
+    <Paper>
+      <Box display="Flex" flexDirection="row">
+        <Avatar>G</Avatar>
+        <Box display="Flex" flexDirection="column">
+          <Typography>Name</Typography>
+          <Button>Join</Button>
+        </Box>
+      </Box>
+    </Paper>
+  );
 }
 
 export default CardGroup;
