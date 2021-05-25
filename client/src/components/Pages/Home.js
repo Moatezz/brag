e02e99c8642ab081ef55c4224e813af96e815734
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-25 11:28:43 +0100

    a- tabs

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navigation from '../Navigation';
+
+import { Box, makeStyles, Paper, Tabs, Tab } from '@material-ui/core';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing(9),
+    [theme.breakpoints.down('md')]: {
+      marginTop: theme.spacing(12),
+    },
+  },
+}));
 
 function Home() {
-  return <div></div>;
+  const classes = useStyle();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box>
+      <Navigation />
+      <Paper className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Joined" />
+          <Tab label="Owned" />
+          <Tab label="Public" />
+        </Tabs>
+      </Paper>
+    </Box>
+  );
 }
 
 export default Home;
