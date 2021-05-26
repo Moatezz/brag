1b9284526b277b52c3e0249db2c18e1fb2fc00ef
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-26 22:31:06 +0100

    added useReducer every thing works well

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,19 @@
-import { Box } from '@material-ui/core';
-import React from 'react';
+import { Box, Drawer, IconButton, Typography } from '@material-ui/core';
+import React, { useReducer } from 'react';
+import { CgProfile } from 'react-icons/cg';
 
-function Profile() {
-  return <Box></Box>;
+function Profile({ icons }) {
+  const [state, dispatch] = useReducer((state) => !state, false);
+  return (
+    <Box>
+      <IconButton>
+        <CgProfile className={icons} onClick={dispatch} />
+      </IconButton>
+      <Drawer anchor="left" open={state} onClose={dispatch}>
+        <Typography>something</Typography>
+      </Drawer>
+    </Box>
+  );
 }
 
 export default Profile;
