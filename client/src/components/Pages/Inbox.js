9479dedef5302b9b954bfa0ca46fd49de9db04f8
Moatezz Chabane <moatezzchabane@gmail.com>
2021-05-29 14:21:48 +0100

    Popup working fine

--- client/src/components/Pages/Inbox.js
+++ client/src/components/Pages/Inbox.js
@@ -1,7 +1,30 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-function Inbox() {
-  return <Box></Box>;
+import React, { useReducer } from 'react';
+import {
+  Box,
+  IconButton,
+  makeStyles,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+} from '@material-ui/core';
+import { BsFillInboxFill } from 'react-icons/bs';
+const useStyle = makeStyles((theme) => ({
+  root: {},
+}));
+function Inbox({ className }) {
+  const [state, handleClick] = useReducer((state) => !state, false);
+  const classes = useStyle();
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <BsFillInboxFill className={className} />
+      </IconButton>
+      <Dialog className={classes.root} open={state} onClose={handleClick}>
+        <DialogTitle className={classes.title}>Inbox</DialogTitle>
+        <DialogContent className={classes.mainContainer}>Hi</DialogContent>
+      </Dialog>
+    </>
+  );
 }
 
 export default Inbox;
